f9de869c70279e2cdb2dbdd193e30ceb7641f551
Kotebus666 <kotebus666@gmail.com>
2025-06-17 17:45:45 +0300

    We are in the vite now! Viva la vite!

--- src/components/ToastPlayground/ToastPlayground.tsx
+++ src/components/ToastPlayground/ToastPlayground.tsx
@@ -11,7 +11,7 @@ function ToastPlayground() {
   return (
     <div className={styles.wrapper}>
       <header>
-        <img alt="Cute toast mascot" src="/toast.png" />
+        <img alt="Cute toast mascot" src="/assets/toast.png" />
         <h1>Toast Playground</h1>
       </header>
 
